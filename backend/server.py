cca49b56fc7335e44932ecec105190998c35ccb9
AbdallahAlii <AbdullahiAliAbdullahi10@gmail.com>
2026-02-16 22:38:49 +0300

    updated port number to 700

--- backend/server.py
+++ backend/server.py
@@ -5,6 +5,6 @@ app = create_app()
 
 if __name__ == "__main__":
     host = os.getenv("HOST", "0.0.0.0")
-    port = int(os.getenv("PORT", "9000"))
+    port = int(os.getenv("PORT", "7000"))
     debug = os.getenv("DEBUG", "1") == "1"
     app.run(host=host, port=port, debug=debug)
